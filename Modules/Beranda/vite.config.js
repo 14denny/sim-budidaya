a8f9eb7cbdf10ca821fbdce1bec5f9b7dca9f485
Denny Syaputra <syaputradenny911@gmail.com>
2022-11-03 21:03:10 +0700

    initial commit

---
+++ Modules/Beranda/vite.config.js
@@ -0,0 +1,24 @@
+const dotenvExpand = require('dotenv-expand');
+dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    build: {
+        outDir: '../../public/build-beranda',
+        emptyOutDir: true,
+        manifest: true,
+    },
+    plugins: [
+        laravel({
+            publicDirectory: '../../public',
+            buildDirectory: 'build-beranda',
+            input: [
+                __dirname + '/Resources/assets/sass/app.scss',
+                __dirname + '/Resources/assets/js/app.js'
+            ],
+            refresh: true,
+        }),
+    ],
+});
